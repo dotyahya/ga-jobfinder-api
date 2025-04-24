ce0cbac0c3bbeae312dd37bc4f5bb206d793455f
Muhammad Yahya <130864895+dotyahya@users.noreply.github.com>
2025-04-24 18:02:03 +0500

    feat: implement rate limiting for job search endpoint and add missing files to .gitignore

--- backend/routers/jobfinder.py
+++ backend/routers/jobfinder.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from backend.schemas.job_request import JobRequest
 from backend.schemas.job_response import JobResponse
-from backend.services.scraper import scrape_rozee_jobs, scrape_jobs
+from backend.services.scraper import scrape_jobs
+from backend.services.rate_limiter import limiter
 
 router = APIRouter()
 
@@ -13,7 +14,8 @@ router = APIRouter()
 #     }
 
 @router.post("/search", response_model=JobResponse)
-async def find_jobs(request: JobRequest):
+@limiter.limit("5/month")
+async def find_jobs(job_request: JobRequest, request: Request):
     # jobs = scrape_indeed_jobs(request.position, request.location)
     # return {
     #     "relevant_jobs": jobs
@@ -24,7 +26,7 @@ async def find_jobs(request: JobRequest):
     #     "relevant_jobs": jobs
     # }
 
-    criteria = JobRequest(**request.model_dump())
+    criteria = JobRequest(**job_request.model_dump())
     # relevant_jobs = await scrape_rozee_jobs(criteria)
     relevant_jobs = await scrape_jobs(criteria)
     return relevant_jobs
